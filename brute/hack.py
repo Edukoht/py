4d1b77a925ad0ba6786dce434b537bd26ab07d91
Edukoht <67044494+Edukoht@users.noreply.github.com>
2022-05-20 19:32:53 +0300

    Update hack.py

--- brute/hack.py
+++ brute/hack.py
@@ -11,13 +11,10 @@ possible_symbols = "abcdefghijklmnopqrstuvwxyz_-0123456789"     # + "ABCDEFGHIJK
 first_symbol = possible_symbols[0]      # Тут хранится буква "а" (она маленькая!)
 last_symbol = possible_symbols[len(possible_symbols) - 1]      # а тут - цифра 9
 
-time = datetime.now() # Засекаем время
+timer = datetime.now() # Засекаем время
 
-with open('hacker_secret.txt') as f:
-
-    lines = f.readlines() 
-    hacker_secret_to_hack = lines[0] # Тут программа считывает, что именно вы загадали
-
+with open('brute/hacker_secret.txt') as f:
+    hacker_secret_to_hack = f.read() # Тут программа считывает, что именно вы загадали
 
     guess = first_symbol
     current_length = 1 # А тут - делает первое предположение: букву "а"
@@ -44,4 +41,4 @@ with open('hacker_secret.txt') as f:
                     break
 
     print("Вы взломаны! Ваш секрет был: " + guess)
-    print("Времени ушло: " + str(datetime.now() - time))
+    print("Времени ушло: " + str(datetime.now() - timer))
