58994eff146d2335a7f5eb0ae356f5912bea6032
Edukoht <67044494+Edukoht@users.noreply.github.com>
2022-05-20 21:00:51 +0300

    Update hack.py

--- brute/hack.py
+++ brute/hack.py
@@ -16,13 +16,14 @@ timer = datetime.now() # Засекаем время
 with open('brute/hacker_secret.txt') as f:
     hacker_secret_to_hack = f.read() # Тут программа считывает, что именно вы загадали
 
-    guess = first_symbol
+    guess = ""
     current_length = 1 # А тут - делает первое предположение: букву "а"
 
     while guess != hacker_secret_to_hack:
         time.sleep(0.05) # Это выводятся предположения в консоль снизу. Удалите эту строку для скорости!
         print(guess) # Для суперскорости можно удалить и эту строку, но тогда вы не будете видеть, что сейчас проверяет код
         
+        if not (guess): guess = first_symbol
 
         # Всё, что ниже - это алгоритм поиска, который каждый раз проверяет следующее значение
         if guess[0] == last_symbol:
